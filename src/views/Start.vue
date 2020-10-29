bcd1b895f02b89fc60872777dd8d91d0ecbd53fc
Anzelika Häberli <anzelika@joineer.ch>
2020-10-29 18:23:15 +0100

    test

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card class="mx-auto mt-10" width="200">
-    <v-card-text class="text-center">START</v-card-text>
+    <v-card-text class="text-center">RISK MANAGEMENT</v-card-text>
   </v-card>
 </template>
 
